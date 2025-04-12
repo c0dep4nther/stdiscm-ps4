467e0e79d99d7b05c5cd237e9ac55ce51d2ff2cf
Mark Daniel Gutierrez <markdcg@outlook.com>
2025-04-13 00:26:03 +0800

    chore: centralize docker to root directory

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    AUTH_SERVICE_URL: process.env.AUTH_SERVICE_URL || "http://localhost:5001",
+    COURSE_SERVICE_URL:
+      process.env.COURSE_SERVICE_URL || "http://localhost:5002",
+    GRADE_SERVICE_URL: process.env.GRADE_SERVICE_URL || "http://localhost:5003",
+  },
+  // Other Next.js config options
+};
 
 export default nextConfig;
