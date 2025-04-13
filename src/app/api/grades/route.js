3bacc5aa48211145e80fcafc19d70b81989520b7
Mark Daniel Gutierrez <markdcg@outlook.com>
2025-04-13 13:39:24 +0800

    fix: routing to fetch grades

--- src/app/api/grades/route.js
+++ src/app/api/grades/route.js
@@ -1,5 +1,6 @@
 export async function GET() {
-  const response = await fetch("http://localhost:5003/api/grades/getAll");
+  const baseUrl = process.env.GRADE_SERVICE_URL;
+  const response = await fetch(`${baseUrl}/api/grades/getAll`);
   const courses = await response.json();
   return Response.json(courses);
 }
